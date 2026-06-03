76276067afe0fab47111a6584b0060821058f508
Anonymous9x-oss <bopaldwi@gmail.com>
2026-06-03 20:51:46 +0700

    thiss

--- Anonymous9xOwner3.lua
+++ Anonymous9xOwner3.lua
@@ -63,7 +63,8 @@ local Config = {
         {Name = "Full Rtx", Icon = "Misc", URL = "https://raw.githubusercontent.com/Anonymous9x-oss/Anonymous9xProjectv1/refs/heads/main/Rtx.lua", Desc = "full preset hd with filters like rtx panel"},
         {Name = "Spiderman Walk", Icon = "Main", URL = "https://raw.githubusercontent.com/Anonymous9x-oss/Anonymous9xProjectv1/refs/heads/main/Walkonwall.lua", Desc = "FE can run on walls, etc., without ui"},
         {Name = "Backdoor Tester", Icon = "Chaos", URL = "https://raw.githubusercontent.com/Anonymous9x-oss/Anonymous9xProjectv1/refs/heads/main/Scan%20backdoor.lua", Desc = "tools and backdoor scan tests, if there is a backdoor, you can run require"},
-        {Name = "Blox Fruit", Icon = "VIP", URL = "https://raw.githubusercontent.com/Anonymous9x-oss/Anonymous9xProjectv1/refs/heads/main/bf.lua", Desc = "Premium blox fruit script with complete features"}
+        {Name = "Blox Fruit", Icon = "VIP", URL = "https://raw.githubusercontent.com/Anonymous9x-oss/Anonymous9xProjectv1/refs/heads/main/bf.lua", Desc = "Premium blox fruit script with complete features"},
+        {Name = "Evade", Icon = "VIP", URL = "https://raw.githubusercontent.com/Anonymous9x-oss/Anonymous9xProjectv1/refs/heads/main/evade.lua", Desc = "Premium Evade script with complete features"} 
     }
 }
 
